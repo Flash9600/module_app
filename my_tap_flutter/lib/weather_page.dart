3fb24708ac13785b658090d2e9e10a77f8ddb5a4
Flash9600 <fds@>
2024-01-22 19:25:29 +0400

    add flutter activity to android app

---
+++ my_tap_flutter/lib/weather_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class WeatherPage extends StatelessWidget {
+  WeatherPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello, my tap weather'),
+    );
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,18 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
-        mavenCentral()
+        mavenCentral();
+        maven {
+            url = uri("my_tap_flutter/build/host/outputs/repo")
+        };
+        maven {
+            url = uri("https://storage.googleapis.com/download.flutter.io")
+        }
     }
 }
 
 rootProject.name = "module_app"
 include(":app")
- 
